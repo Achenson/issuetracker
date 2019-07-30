424cd35fe9d41c9093c0d14355358e1615a6d53f
achenson <45999022+Achenson@users.noreply.github.com>
2019-07-30 16:04:57 +0200

    mongoose installed

--- routes/api.js
+++ routes/api.js
@@ -11,8 +11,17 @@
 var expect = require('chai').expect;
 var MongoClient = require('mongodb');
 var ObjectId = require('mongodb').ObjectID;
+var mongoose = require('mongoose');
 
-const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+//const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+
+const CONNECTION_STRING =
+  "mongodb+srv://Achenson:dsf3Z1IKO1GCEZtv@mongo-for-fcc-13gh5.mongodb.net/test?retryWrites=true&w=majority";
+
+mongoose
+  .connect(CONNECTION_STRING, { useNewUrlParser: true })
+  .then(() => console.log("connection succesfull"))
+  .catch(err => console.log(err));
 
 module.exports = function (app) {
 
